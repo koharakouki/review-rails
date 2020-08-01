5f2bf841fa55ca8fcaaf4c7391f4d8edf5f6d154
koharakouki <ko.kohara02@gmail.com>
2020-08-01 07:05:02 +0000

    validation index callback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_052615) do
+ActiveRecord::Schema.define(version: 2020_08_01_070113) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
