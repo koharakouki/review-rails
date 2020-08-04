a46ea57a327fec14aae1b3b7c2618c7088617f74
koharakouki <ko.kohara02@gmail.com>
2020-08-04 01:42:33 +0000

    fin chapter9-1-3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,14 +22,21 @@ module SessionsHelper
     end
   end
 
-  end
-
   def logged_in?
     !current_user.nil?
   end
+  
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
+
+
 end
